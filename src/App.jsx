6f085e1ee9615e6cc8531752f0e2fcb1d2e5b7aa
tara05ma <mandyamtara@gmail.com>
2026-02-02 10:05:55 +0530

    R3F basic cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { use, useState } from 'react'
 import './App.css'
+import { Canvas,useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+
+const Cube = ({position,size,colour}) =>{
+  const ref = useRef()
+  useFrame((state,delta) => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+    ref.current.rotation.z += 0.05
+    ref.current.position.z = Math.sin(state.clock.elapsedTime)
+  })
+  return(
+    <mesh position={position} ref={ref}>
+      <boxGeometry args={size}/> 
+      <meshStandardMaterial color={colour}/>
+    </mesh>
+  )
+
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://youtube.com" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Rubick's Cube Solver</h1>
-      <h2>Welcome to Rubix, The only solver you need.</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick ={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}>
-          You're GAY.
-        </button>
-      </div>
-      <p className="read-the-docs" onClick={() => window.open("https://www.instagram.com/krishankkureti")}>
-        Click on this to learn more
-      </p>
-    </>
+    <Canvas>
+      <ambientLight position={[0, 10, 5]} intensity={1}/> 
+      <Cube position = {[-1,1,1]} size = {[1,1,1]} colour={"purple"}/>
+      <Cube position = {[2,2,1]} size = {[1,1,1]} colour={"orange"}/>
+    </Canvas>
   )
 }
 
